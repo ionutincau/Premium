7be6a14b212205f32532c3e027739efc1e62500c
ionutincau <ionut.incau@gmail.com>
2017-02-28 18:18:45 +0200

    Revert "Initial commit"
    This reverts commit 71a1e5998383eea1c8ddcf2cf0a70d3fab279692.
    
    # Conflicts:
    #	README.md

--- README.md
+++
@@ -1,3 +0,0 @@
-# Premium
-Team project
-Bagati link in chrome : https://www.youtube.com/watch?v=Eb2W0O53A2U
--- src/Application.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Incau Ionut on 28-Feb-17.
- * Contact: ionut.incau@gmail.com
- */
-public class Application {
-}
