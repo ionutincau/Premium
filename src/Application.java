ad9bd0bc6ff5f9ddb65af295c7020bc1fef0bdf2
ionutincau <ionut.incau@gmail.com>
2017-02-28 18:22:57 +0200

    Add IntelliJ
    Start Over

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Incau Ionut on 28-Feb-17.
+ * Contact: ionut.incau@gmail.com
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
