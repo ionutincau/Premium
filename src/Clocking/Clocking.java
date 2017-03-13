6538627f2364200bf0aecb5ca466246d51c04aef
MariusDK <mariusdk7@gmail.com>
2017-03-13 22:40:49 +0200

    Add Clocking

---
+++ src/Clocking/Clocking.java
@@ -0,0 +1,21 @@
+package Clocking;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by MariusDK on 13.03.2017.
+ */
+public class Clocking {
+    
+    private GregorianCalendar hour_in = new GregorianCalendar();
+    private GregorianCalendar hour_break = new GregorianCalendar();
+    private GregorianCalendar hour_work = new GregorianCalendar();
+    private GregorianCalendar hour_out = new GregorianCalendar();
+
+
+
+
+
+
+
+}
