a706e38e3e4ca5316cb8dec11504b49e1c40e7f6
ionutincau <ionut.incau@gmail.com>
2017-03-14 15:50:13 +0200

    Update ClockingUI Design

--- src/Clocking/ClockingController.java
+++ src/Clocking/ClockingController.java
@@ -3,5 +3,10 @@ package Clocking;
 /**
  * Created by MariusDK on 13.03.2017.
  */
+
 public class ClockingController {
+
+    public void getClocking() {
+        //TODO: return a list of clockings
+    }
 }
