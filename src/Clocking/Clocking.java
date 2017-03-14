51cc09a895a0abc7fdcee062c88100506e2d3542
ionutincau <ionut.incau@gmail.com>
2017-03-14 10:43:40 +0200

    Modify Clocking Add Login Add tests

--- src/Clocking/Clocking.java
+++ src/Clocking/Clocking.java
@@ -8,13 +8,23 @@ import java.util.GregorianCalendar;
  */
 
 public class Clocking {
-    
-    private Calendar hour_in = new GregorianCalendar(); // clock-in start work
-    private Calendar hour_break = new GregorianCalendar(); // clock-out for break
-    private Calendar hour_work = new GregorianCalendar(); // clock-in after break
-    private Calendar hour_out = new GregorianCalendar(); // clock-out
 
-    public Clocking (GregorianCalendar hour_in, GregorianCalendar hour_break, GregorianCalendar hour_work, GregorianCalendar hour_out) {
+    private Calendar date = new GregorianCalendar(); // date
+    private int hour_in; // number of minutes of day - clock-in start work
+    private int hour_break; // number of minutes of day - clock-out for break
+    private int hour_work; // number of minutes of day  - clock-in after break
+    private int hour_out; // number of minutes of day - clock-out
+
+    public Clocking(Calendar date, int hour_in) {
+        this.date = date;
+        this.hour_in = hour_in;
+        this.hour_break = 0;
+        this.hour_work = 0;
+        this.hour_out = 0;
+    }
+
+    public Clocking (Calendar date, int hour_in, int hour_break, int hour_work, int hour_out) {
+        this.date = date;
         this.hour_in = hour_in;
         this.hour_break = hour_break;
         this.hour_work = hour_work;
@@ -22,38 +32,53 @@ public class Clocking {
     }
 
     public Calendar get_date() {
+        return this.date;
+    }
+
+    public int get_hour_in() {
         return this.hour_in;
     }
 
-    public Calendar get_hour_in() {
-        return this.hour_in;
+    public int get_hour_break() {
+        return this.hour_break;
     }
 
-    public Calendar get_hour_break() {
-    return this.hour_break;
-    }
-
-    public Calendar get_hour_work() {
+    public int get_hour_work() {
         return this.hour_work;
     }
 
-    public Calendar get_hour_out() {
+    public int get_hour_out() {
         return this.hour_out;
     }
 
-    public void set_hour_in(Calendar calendar) {
-        this.hour_in = calendar;
+    public void set_date(Calendar date) {
+        this.date = date;
     }
 
-    public void set_hour_break(Calendar calendar) {
-        this.hour_break = calendar;
+    public void set_hour_in(int hour_in) {
+        this.hour_in = hour_in;
     }
 
-    public void set_hour_work(Calendar calendar) {
-        this.hour_work = calendar;
+    public void set_hour_break(int hour_break) {
+        this.hour_break = hour_break;
     }
 
-    public void set_hour_out(Calendar calendar) {
-        this.hour_out = calendar;
+    public void set_hour_work(int hour_work) {
+        this.hour_work = hour_work;
+    }
+
+    public void set_hour_out(int hour_out) {
+        this.hour_out = hour_out;
+    }
+
+    /**
+     * @return number of worked minutes in a day
+     */
+    public int get_time() {
+        if (hour_out == 0) {
+            if (hour_break == 0) return 0;
+            return hour_break - hour_in;
+        }
+        return hour_break - hour_in + hour_out - hour_work;
     }
 }
---
+++ tests/Clocking/ClockingTest.java
@@ -0,0 +1,35 @@
+package Clocking;
+
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Incau Ionut on 14-Mar-17.
+ * Contact: ionut.incau@gmail.com
+ */
+
+public class ClockingTest {
+
+    @Test
+    public void get_time() throws Exception {
+        GregorianCalendar date = new GregorianCalendar();
+
+        Clocking clocking = new Clocking(date, 480);
+        assertTrue(clocking.get_time() == 0);
+        clocking.set_hour_break(500);
+        assertTrue(clocking.get_time() == 20);
+        clocking.set_hour_work(600);
+        assertTrue(clocking.get_time() == 20);
+        clocking.set_hour_out(620);
+        assertTrue(clocking.get_time() == 40);
+
+        Clocking clocking2 = new Clocking(date, 200);
+        assertTrue(clocking2.get_time() == 0);
+        clocking2.set_hour_out(300);
+        assertTrue(clocking2.get_time() == 100);
+    }
+
+}
