7ee751ea03c524a46452daee3e931d34f9a2962f
Aurelian <thefrozenone95@gmail.com>
2017-03-19 22:45:48 +0200

    Created LoginUI

--- src/Login/LoginController.java
+++ src/Login/LoginController.java
@@ -14,14 +14,48 @@ public class LoginController {
 
     public LoginController(Stage stage) {
         this.primaryStage = stage;
-        //TODO: first set loginUI, and, after user is logged in setScene()
+        //TODO: first set loginUI, and, after user is logged in setLoginScene()
+
     }
 
-    public void setScene() {
+    public void setLoginScene() {
         // TODO: choose between user, hr, or admin stage
 
+        LoginUI loginUI = new LoginUI();
+        Scene loginScene = loginUI.getScene();
+        this.primaryStage.setScene(loginScene);
+
+        /*
+        ClockingUI clockingUI = new ClockingUI();
+        Scene scene = clockingUI.getScene();
+        this.primaryStage.setLoginScene(scene);
+        */
+    }
+
+    public void setUserScene()
+    {
         ClockingUI clockingUI = new ClockingUI();
         Scene scene = clockingUI.getScene();
         this.primaryStage.setScene(scene);
+    }
+
+    public void setHRScene()
+    {
+
+    }
+
+    public void setAdminScene()
+    {
+
+    }
+
+    public void login(String username, String password)
+    {
+        //TODO replace code below after database is up
+        if (username.equals("admin") && password.equals("123456"))
+        {
+            this.setUserScene();
+
+        }
     }
 }
